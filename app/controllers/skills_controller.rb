7d677a3a9d37b4248942b8624af622c474bc1e13
lewis nyoike <nyoikelewis@gmail.com>
2017-08-04 15:12:26 +0300

    Defined a create method to save the new skill

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -9,3 +9,12 @@ class SkillsController < ApplicationController
    render :new
   end
 
+  def create
+      @skill = Skill.new(skill_params)
+      if @skill.save
+        redirect_to  skills_path
+      else
+        render :new
+      end
+  end
+
