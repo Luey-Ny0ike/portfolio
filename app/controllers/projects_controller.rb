bc038942384d957858d02738da8044493f7b4d8e
lewis nyoike <nyoikelewis@gmail.com>
2017-08-05 19:49:36 +0300

    Method to add the project to the db

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,3 +2,12 @@
       @skill = Skill.find(params[:skill_id])
       @project = @skill.projects.new
     end
+    def create
+     @skill = Skill.find(params[:skill_id])
+     @project = @skill.projects.new(project_params)
+     if @project.save
+       redirect_to skill_path(@project.skill)
+     else
+       render :new
+     end
+    end
