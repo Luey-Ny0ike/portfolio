589a2c64773945a80232e83187e095f64c862ba0
lewis nyoike <nyoikelewis@gmail.com>
2017-08-05 19:50:17 +0300

    defined a method to edit and update a project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -11,3 +11,18 @@
        render :new
      end
     end
+
+    def edit
+        @skill = Skill.find(params[:skill_id])
+        @project = Project.find(params[:id])
+    end
+
+    def update
+        @skill= Skill.find(params[:skill_id])
+        @project = Project.find(params[:id])
+        if @skill.projects.update(project_params)
+          redirect_to skill_path(@project.skill)
+        else
+          render :edit
+        end
+    end
