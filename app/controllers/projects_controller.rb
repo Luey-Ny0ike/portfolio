7965e8c7bb1f88ed228b66e7ddbb914e28b6db67
lewis nyoike <nyoikelewis@gmail.com>
2017-08-14 20:19:32 +0300

    Defined methods to add delete and update comments on blogs

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,42 +1,44 @@
 class ProjectsController < ApplicationController
-    def new
-      @skill = Skill.find(params[:skill_id])
-      @project = @skill.projects.new
-    end
-    def create
-     @skill = Skill.find(params[:skill_id])
-     @project = @skill.projects.new(project_params)
-     if @project.save
-       redirect_to skill_path(@project.skill)
-     else
-       render :new
-     end
-    end
+  def new
+    @skill = Skill.find(params[:skill_id])
+    @project = @skill.projects.new
+  end
 
-    def edit
-        @skill = Skill.find(params[:skill_id])
-        @project = Project.find(params[:id])
+  def create
+    @skill = Skill.find(params[:skill_id])
+    @project = @skill.projects.new(project_params)
+    if @project.save
+      redirect_to skill_path(@project.skill)
+    else
+      render :new
     end
+  end
 
-    def update
-        @skill= Skill.find(params[:skill_id])
-        @project = Project.find(params[:id])
-        if @skill.projects.update(project_params)
-          redirect_to skill_path(@project.skill)
-        else
-          render :edit
-        end
+  def edit
+    @skill = Skill.find(params[:skill_id])
+    @project = Project.find(params[:id])
+  end
+
+  def update
+    @skill = Skill.find(params[:skill_id])
+    @project = Project.find(params[:id])
+    if @skill.projects.update(project_params)
+      redirect_to skill_path(@project.skill)
+    else
+      render :edit
     end
+  end
 
-    def destroy
-        @skill = Skill.find(params[:skill_id])
-        @project = Project.find(params[:id])
-        @project.destroy
-        redirect_to skill_path(@project.skill)
-    end
+  def destroy
+    @skill = Skill.find(params[:skill_id])
+    @project = Project.find(params[:id])
+    @project.destroy
+    redirect_to skill_path(@project.skill)
+  end
 
- private
-   def project_params
-     params.require(:project).permit(:name, :description, :link, :image)
-   end
+  private
+
+  def project_params
+    params.require(:project).permit(:name, :description, :link, :image)
+  end
 end
