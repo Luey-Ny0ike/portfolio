59f1264752488d4ffe9d38f3f24540309d26aca4
lewis nyoike <nyoikelewis@gmail.com>
2017-08-15 23:58:15 +0300

    Used devise to only allow loged in admins to add a new blog post

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -15,7 +15,7 @@ class BlogsController < ApplicationController
 
   # GET /blogs/new
   def new
-    @blog = Blog.new
+    @blog = current_admin.blogs.build
   end
 
   # GET /blogs/1/edit
@@ -25,7 +25,7 @@ class BlogsController < ApplicationController
   # POST /blogs
   # POST /blogs.json
   def create
-    @blog = Blog.new(blog_params)
+    @blog = @blog = current_admin.blogs.build(blog_params)
 
     respond_to do |format|
       if @blog.save
