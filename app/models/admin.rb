ffa68d37c1ce402ad6b42a900a124ea79246569f
lewis nyoike <nyoikelewis@gmail.com>
2017-08-15 23:56:06 +0300

    Made the relation between blogs and an admin

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,4 +1,5 @@
 class Admin < ApplicationRecord
+  has_many :blogs
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
