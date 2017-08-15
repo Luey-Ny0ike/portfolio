be754cb906fedcb52bbe1821589c9ead139be152
lewis nyoike <nyoikelewis@gmail.com>
2017-08-15 21:53:09 +0300

    Added flash alerts and notices when a comment is added or edited

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -26,8 +26,10 @@ class CommentsController < ApplicationController
     @blog = Blog.find(params[:blog_id])
     @comment = @blog.comments.new(comment_params)
     if @comment.save
+       flash[:notice] = "Comment added successfully"
       redirect_to blog_comment_path(@comment.blog)
     else
+      flash[:alert] = "Comment was not added"
       render :new
     end
   end
@@ -41,8 +43,10 @@ class CommentsController < ApplicationController
     @blog = Blog.find(params[:blog_id])
     # @comment = @blog.comments.find(params[:id])
     if @blog.comments.update(comment_params)
+      flash[:notice] = "Comment updated successfully"
       redirect_to blog_comments_path(@comment.blog)
     else
+      flash[:alert] = "Comment was not updated!"
       render :edit
     end
   end
@@ -54,13 +58,13 @@ class CommentsController < ApplicationController
     redirect_to blog_comments_path(@comment.blog)
   end
 
+  def set_comment
+    @comment = Comment.find(params[:id])
+  end
+
   private
 
   def comment_params
     params.require(:comment).permit(:comment)
-  end
-
-  def set_comment
-    @comment = Comment.find(params[:id])
   end
 end
