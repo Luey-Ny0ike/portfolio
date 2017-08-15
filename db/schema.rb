23b6657fd1a94bcfd758c185561383ca01b68fe4
lewis nyoike <nyoikelewis@gmail.com>
2017-08-15 22:36:51 +0300

    Created a new column in the blogs table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170815185321) do
+ActiveRecord::Schema.define(version: 20170815192500) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 20170815185321) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "comment_id"
+    t.integer "admin_id"
   end
 
   create_table "comments", force: :cascade do |t|
