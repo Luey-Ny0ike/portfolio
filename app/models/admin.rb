a3ae9e238f7247f3866b45b1a42c7e1f29d26895
lewis nyoike <nyoikelewis@gmail.com>
2017-08-24 10:12:47 +0300

    Removed any traces of previous admin configuration

--- app/models/admin.rb
+++
@@ -1,7 +0,0 @@
-class Admin < ApplicationRecord
-  has_many :blogs
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-end
