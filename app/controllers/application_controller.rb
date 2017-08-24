b01c398eea7e44fe38b99a6f84a4ee573af955e5
lewis nyoike <nyoikelewis@gmail.com>
2017-08-24 12:50:15 +0300

    Added a method to catch all the erroes and display an error message instead

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+rescue_from CanCan::AccessDenied do |exception|
+  redirect_to main_app.new_user_session_path, alert: exception.message
+end
 
   def after_sign_in_path_for(user)
       blogs_path
