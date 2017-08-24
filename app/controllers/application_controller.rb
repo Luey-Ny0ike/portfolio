1a641f2575d696be11dafa04ae9a7701c20a8115
lewis nyoike <nyoikelewis@gmail.com>
2017-08-24 12:58:13 +0300

    Sorted the log in and out issue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,13 +4,9 @@ rescue_from CanCan::AccessDenied do |exception|
   redirect_to main_app.new_user_session_path, alert: exception.message
 end
 
-  def after_sign_in_path_for(user)
-      blogs_path
-  end
-
-  def after_sign_out_path_for(user)
-      blogs_path
-  end
+def after_sign_out_path_for(user)
+    blogs_path
+end
 
   def after_sign_up_path_for(user)
       blogs_path
