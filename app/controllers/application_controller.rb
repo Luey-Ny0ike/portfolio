30965f55ba151eb0bfc3aa8b411f3f12fd248e40
lewis nyoike <nyoikelewis@gmail.com>
2017-08-24 10:11:30 +0300

    Defined methods to redirect after signing in and out or up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def after_sign_in_path_for(user)
+      blogs_path
+  end
+
+  def after_sign_out_path_for(user)
+      blogs_path
+  end
+
+  def after_sign_up_path_for(user)
+      blogs_path
+  end
 end
